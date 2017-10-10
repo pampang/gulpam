44bf7347a6b08612fd8f7d7813cc6a2e0105c123
龐校長 <fspwz@foxmail.com>
2017-10-10 16:06:06 +0800

    add hash task into pack

--- tasks/pack.js
+++ tasks/pack.js
@@ -1,13 +1,13 @@
 /*
  * @Author: PAMPANG
  * @Date: 2017-08-07 15:13:46
- * @Last Modified by:   PAMPANG
- * @Last Modified time: 2017-08-07 15:13:46
+ * @Last Modified by: PAMPANG
+ * @Last Modified time: 2017-10-10 16:05:47
  */
 var gulp 		= require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('pack', function (cb) {
   console.log('pack start');
-	runSequence('del', 'build', 'minify', 'inlinesource', cb);
+	runSequence('del', 'build', 'minify', 'inlinesource', 'hash', cb);
 });
