e3b345c08b6557c1df8ad212d962fdfaacf24242
龐校長 <fspwz@foxmail.com>
2018-01-02 15:01:17 +0800

    improve `hash` task

--- tasks/hash.js
+++ tasks/hash.js
@@ -2,7 +2,7 @@
  * @Author: PAMPANG
  * @Date: 2017-10-10 15:48:55
  * @Last Modified by: PAMPANG
- * @Last Modified time: 2017-10-10 15:49:30
+ * @Last Modified time: 2017-10-12 14:31:48
  */
 
 const gulp = require('gulp');
@@ -11,8 +11,12 @@ const revts = require('gulp-rev-timestamp');
 const config = require('../config.js');
 
 // add `?rev=@hash` at the end of file url
-gulp.task('hash', function () {
-	gulp.src(config.hash.src)
-		.pipe(revts())
-		.pipe(gulp.dest(config.hash.dest));
+gulp.task('hash', function (cb) {
+	setTimeout(function() {
+		gulp.src(config.hash.src)
+			.pipe(revts())
+			.pipe(gulp.dest(config.hash.dest));
+
+		cb();
+	}, 1000);
 });
