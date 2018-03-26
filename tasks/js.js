a0e5ab7c63a4efd6218ffb35a56dccffc783a935
龐校長 <fspwz@foxmail.com>
2018-03-26 15:58:22 +0800

    [js] 回滚 js 任务，不使用 webpack

--- tasks/js.js
+++ tasks/js.js
@@ -2,19 +2,38 @@
  * @Author: PAMPANG
  * @Date: 2016-02-16 10:40:23
  * @Last Modified by: PAMPANG
- * @Last Modified time: 2018-02-27 18:25:56
+ * @Last Modified time: 2017-08-07 15:13:17
  */
-var gulp = require('gulp');
-var webpack = require('webpack');
-var path = require('path');
-var process = require('process');
-var webpackConfig = require(path.resolve(process.cwd(), 'webpack.config'));
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const cache = require('gulp-cached');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
 
-gulp.task('js', (cb) => {
-  webpack(webpackConfig, (error, stats) => {
-    if (error) {
-      console.log(error);
-    }
-    cb();
-  });
+const config = require('../config.js');
+
+gulp.task('js', () => {
+  console.log('js start');
+  return gulp
+    .src(config.js.src)
+    .pipe(gulpif(global.__DEV__, sourcemaps.init()))
+    .pipe(plumber({
+      errorHandler(error) {
+        console.log(error.message);
+        this.emit('end');
+      },
+    }))
+    .pipe(babel({
+      presets: [
+        ['env', {
+          targets: {
+            browsers: ['last 10000 versions', 'safari 6'],
+          },
+        }],
+      ],
+    }))
+    .pipe(cache('js'))
+    .pipe(gulpif(global.__DEV__, sourcemaps.write()))
+    .pipe(gulp.dest(config.js.dest));
 });
