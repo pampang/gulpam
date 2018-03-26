5dd573ef3d6e5628e424fc334a92eaafeeb6c09f
龐校長 <fspwz@foxmail.com>
2018-03-26 18:45:30 +0800

    [del] 优化 del

--- tasks/del.js
+++ tasks/del.js
@@ -2,23 +2,28 @@
  * @Author: PAMPANG
  * @Date: 2017-08-07 15:12:00
  * @Last Modified by: PAMPANG
- * @Last Modified time: 2018-03-05 11:24:32
+ * @Last Modified time: 2018-03-26 17:39:25
  */
 var gulp = require('gulp');
+var fs = require('fs');
+var path = require('path');
 var config = require('../config.js');
 var del = require('del');
 
 gulp.task('del', function(cb) {
   console.log('del start');
+
   // 删除指定文件
   const entryDirs = fs
-    .readdirSync(path.resolve(__dirname, ))
+    .readdirSync(config.srcDir)
     .filter((name) => {
-      const filePath = path.resolve(__dirname, config.srcDir);
+      const filePath = path.resolve(config.srcDir, name);
       return fs.statSync(filePath).isDirectory();
     });
 
-  const delDirs = entryDirs.map(dir => path.resolve(__dirname, config.destDir, dir));
+  const delDirs = entryDirs.map(dir => path.resolve(config.destDir, dir));
 
-  return del.sync(delDirs);
+  del.sync(delDirs);
+
+  cb();
 });
