9ce6c22b80ae1f798c07f5ec92fd763d55e29bc4
龐校長 <fspwz@foxmail.com>
2018-03-26 15:58:10 +0800

    [del] 实现删除文件夹的精准操作

--- tasks/del.js
+++ tasks/del.js
@@ -1,8 +1,8 @@
 /*
  * @Author: PAMPANG
  * @Date: 2017-08-07 15:12:00
- * @Last Modified by:   PAMPANG
- * @Last Modified time: 2017-08-07 15:12:00
+ * @Last Modified by: PAMPANG
+ * @Last Modified time: 2018-03-05 11:24:32
  */
 var gulp = require('gulp');
 var config = require('../config.js');
@@ -10,5 +10,15 @@ var del = require('del');
 
 gulp.task('del', function(cb) {
   console.log('del start');
-  return del(config.clean.src);
+  // 删除指定文件
+  const entryDirs = fs
+    .readdirSync(path.resolve(__dirname, ))
+    .filter((name) => {
+      const filePath = path.resolve(__dirname, config.srcDir);
+      return fs.statSync(filePath).isDirectory();
+    });
+
+  const delDirs = entryDirs.map(dir => path.resolve(__dirname, config.destDir, dir));
+
+  return del.sync(delDirs);
 });
